343ca6944f0858235d9296d5a1d4001f403eed2e
Christina <kris.inferno@gmail.com>
2021-06-15 15:33:17 +0300

    views, url

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,41 +1,37 @@
-from django.core.exceptions import ValidationError
+# from django.core.exceptions import ValidationError
 from django.forms import ModelChoiceField, ModelForm
 from django.contrib import admin
-from django.utils.safestring import mark_safe
+# from django.utils.safestring import mark_safe
 
 
 from .models import *
 
-from PIL import Image
 
+# class NotebookAdminForm(ModelForm):
+#
+#     MIN_RESOLUTION = (400, 400)
+#     MAX_RESOLUTION = (800, 800)
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['image'].help_text = mark_safe(
+#             """'<span style="color:red; font-size:12px;">При загрузке изображения с разршением больше {}x{} оно будет обрезано!</span>'""".format(*Product.MIN_RESOLUTION))
 
-class NotebookAdminForm(ModelForm):
-
-    MIN_RESOLUTION = (400, 400)
-    MAX_RESOLUTION = (800, 800)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = mark_safe(
-            '<span style="color:red; font-size:12px;">Загружайте изображение с минимальным разршением {}x{}</span>'.format(*Product.MIN_RESOLUTION))
-
-    def clean_image(self):
-        image = self.cleaned_data['image']
-        img = Image.open(image)
-        min_height, min_width = Product.MIN_RESOLUTION
-        max_height, max_width = Product.MIN_RESOLUTION
-        if image.size > Product.MAX_IMAGE_SIZE:
-            raise ValidationError('Размер изображения не одлжен привышать 3MB!')
-        if img.height < min_height or img.width < min_width:
-            raise ValidationError('Разрешение изображения меньше минимального!')
-        if img.height > max_height or img.width > max_width:
-            raise ValidationError('Разрешение изображения меньше больше максимального!')
-        return image
+    # def clean_image(self):
+    #     image = self.cleaned_data['image']
+    #     img = Image.open(image)
+    #     min_height, min_width = Product.MIN_RESOLUTION
+    #     max_height, max_width = Product.MIN_RESOLUTION
+    #     if image.size > Product.MAX_IMAGE_SIZE:
+    #         raise ValidationError('Размер изображения не одлжен привышать 3MB!')
+    #     if img.height < min_height or img.width < min_width:
+    #         raise ValidationError('Разрешение изображения меньше минимального!')
+    #     if img.height > max_height or img.width > max_width:
+    #         raise ValidationError('Разрешение изображения меньше больше максимального!')
+    #     return image
 
 
 class NotebookAdmin(admin.ModelAdmin):
-
-    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,12 +1,22 @@
-from PIL import Image
+# import sys
+# from PIL import Image
 
 
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.urls import reverse
+# from django.core.files.uploadedfile import InMemoryUploadedFile
+
+# from io import BytesIO
 
 User = get_user_model()
+
+
+def get_product_url(obj, viewname, model_name):
+    ct_model = obj.__class__._meta.model_name
+    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
 
 
 class MinResolutionErrorException:
@@ -76,17 +86,27 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        image = self.image
-        img = Image.open(image)
-        min_height, min_width = self.MIN_RESOLUTION
-        max_height, max_width = self.MIN_RESOLUTION
-        if img.height < min_height or img.width < min_width:
-            raise MinResolutionErrorException('Разрешение изображения меньше минимального!')
-        if img.height > max_height or img.width > max_width:
-            raise MaxResolutionErrorException('Разрешение изображения меньше больше максимального!')
-        return image
-
+    # def save(self, *args, **kwargs):
+    #     image = self.image
+    #     img = Image.open(image)
+    #     min_height, min_width = self.MIN_RESOLUTION
+    #     max_height, max_width = self.MIN_RESOLUTION
+    #     if img.height < min_height or img.width < min_width:
+    #         raise MinResolutionErrorException('Разрешение изображения меньше минимального!')
+    #     if img.height > max_height or img.width > max_width:
+    #         raise MaxResolutionErrorException('Разрешение изображения меньше больше максимального!')
+        # image = self.image
+        # img = Image.open(image)
+        # new_img = img.convert('RGB')
+        # resized_new_img = new_img.resize((200, 200), Image.ANTIALIAS)
+        # filestream = BytesIO()
+        # resized_new_img.save(filestream, 'JPEG', quality=90)
+        # filestream.seek(0)
+        # name = '{}.{}'.format(*self.image.name.split('.'))
+        # self.image = InMemoryUploadedFile(
+        #     filestream, 'ImageField', name, 'jpeg/image', sys.getsizeof(filestream), None
+        # )
+        super().save(*args, **kwargs)
 
 
 class Notebook(Product):
@@ -100,6 +120,9 @@ class Notebook(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class Smartphone(Product):
@@ -115,6 +138,9 @@ class Smartphone(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class CartProduct(models.Model):
